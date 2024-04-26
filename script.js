3fca4a68122c4063b4150e706cd242761fa32fb0
zenunes <jnunesds2@gmail.com>
2024-04-25 21:36:56 -0300

    localizacao

--- script.js
+++ script.js
@@ -7,6 +7,7 @@ let sliderWidth = document.querySelector('.slider--width').style.width = `calc(1
 // ajustando a altura dos controles de slides - centralizar eles
 document.querySelector('.slider-controls').style.height = `${document.querySelector('.sliders').clientHeight}px`;
 
+document.querySelector('.slider-controls-colection').style.height = `${document.querySelector('.slides--colection').clientHeight}px`;
 
 // gerador de indicadores de slides
 
@@ -64,5 +65,38 @@ function updateMargin() {
 
 }
 
+
+
+
+document.addEventListener('DOMContentLoaded', function () {
+    const sliderContainer = document.querySelector('.slider--colection');
+    const slidesColetion = document.querySelectorAll('.slide--colection');
+
+    let currentIndex = 0;
+    // window estão sendo definidos para garantir 
+    // que estas funções estejam acessíveis globalmente, 
+    // o que é necessário quando você usa onclick diretamente nos elementos HTML
+    window.prevSlide = function () {
+        currentIndex--;
+        if (currentIndex < 0) {
+            currentIndex = slidesColetion.length - 4;
+        }
+        updateSlidePosition();
+    }
+    window.nextSlide = function () {
+        currentIndex++;
+        if (currentIndex > slidesColetion.length - 4) {
+            currentIndex = 0;
+        }
+        updateSlidePosition();
+    }
+
+    function updateSlidePosition() {
+
+        sliderContainer.style.transform = `translateX(-${currentIndex * 260}px)`;
+    }
+    setInterval(nextSlide, 5000);
+});
+
 // configurando o avanço automatico dos slides
-setInterval(goNext, 5000);
+setInterval(goNext, 5000); 
