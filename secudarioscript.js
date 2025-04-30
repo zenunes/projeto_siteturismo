b532f56ab8ada5fc4dac46868031ff758dbd9ac5
José Nunes <jnunesds2@gmail.com>
2025-04-30 19:51:09 -0300

    pagina galeria e cultura

--- secudarioscript.js
+++ secudarioscript.js
@@ -4,3 +4,24 @@ document.querySelectorAll('#menu li a').forEach(link => {
         this.parentElement.classList.add('active');
     });
 });
+
+document.addEventListener('DOMContentLoaded', function() {
+    const backToTopButton = document.getElementById('back-to-top');
+
+    // Mostra/esconde botão baseado no scroll
+    window.addEventListener('scroll', function() {
+        if (window.pageYOffset > 300) {
+            backToTopButton.style.display = 'flex';
+        } else {
+            backToTopButton.style.display = 'none';
+        }
+    });
+
+    // Ação de rolagem suave ao clicar
+    backToTopButton.addEventListener('click', function() {
+        window.scrollTo({
+            top: 0,
+            behavior: 'smooth'
+        });
+    });
+});
